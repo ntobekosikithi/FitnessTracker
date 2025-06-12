eef287a9243e8f68e46aaa17a0f86a7a1b122eca
Ntobeko Sikithi <ntobekosikithi@gmail.com.com>
2025-06-12 22:43:42 +0200

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Ntobeko Sikithi on 2025/06/12.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTrackerTests/FitnessTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerTests.swift
+//  FitnessTrackerTests
+//
+//  Created by Ntobeko Sikithi on 2025/06/12.
+//
+
+import Testing
+@testable import FitnessTracker
+
+struct FitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
